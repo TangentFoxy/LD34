f3c5e2a7b9f49433a8fa65115d04b7cb2542c564
Paul Liverman III <paul.liverman.iii@gmail.com>
2015-12-14 12:10:39 -0800

    fixed random radian generation on several bodies

--- src/Bodies/Asteroid.lua
+++ src/Bodies/Asteroid.lua
@@ -10,17 +10,16 @@ function Asteroid:initialize(range)
     self.type = "Asteroid"
     self.name = "Asteroid " .. name.generate(1, 2):upper() .. "-" .. random(11, 99)
 
-    local direction = random(0, math.pi*2)
+    local direction = random()*math.pi*2
     local distance = random(0, range)
     self.x = distance * math.cos(direction)
     self.y = distance * math.sin(direction)
 
     self.image = random(10, 17)
     self.sx = random(2, 11)
-    --self.sy = self.sx
     self.sy = random(2, 11)
 
-    self.r = random(0, math.pi*2)
+    self.r = random()*math.pi*2
 end
 
 return Asteroid
--- src/Bodies/Debris.lua
+++ src/Bodies/Debris.lua
@@ -9,17 +9,16 @@ function Debris:initialize(range)
     self.type = "Debris"
     self.name = self.name .. " (Debris)"
 
-    local direction = random(0, math.pi*2)
+    local direction = random()*math.pi*2
     local distance = random(0, range)
     self.x = distance * math.cos(direction)
     self.y = distance * math.sin(direction)
 
     self.image = random(19, 20)
     self.sx = random(2, 7)
-    --self.sy = self.sx
     self.sy = random(2, 7)
 
-    self.r = random(0, math.pi*2)
+    self.r = random()*math.pi*2
 end
 
 return Debris
