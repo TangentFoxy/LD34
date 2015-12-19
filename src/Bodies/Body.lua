740c43493a47ca2f642394f58fbae16ae5f362b6
Paul Liverman III <paul.liverman.iii@gmail.com>
2015-12-18 21:13:00 -0800

    started on more features to body

--- src/Bodies/Body.lua
+++ src/Bodies/Body.lua
@@ -2,27 +2,43 @@ local class = require "lib.middleclass"
 local Body = class("Bodies.Body")
 local random = math.random
 
-function Body:initialize()
-    self.type = "Body"
-    self.name = random()
+local lume = require "lib.lume"
 
+function Body:initialize()
+    -- Basics
+    self.type = "Body"
+    self.name = random() --TODO make subclasses modify instead of replace this? use uuid in naming? idk
+    self.uuid = lume.uuid()
+    -- Position
+    --TODO make this (sector) actually set for everything!
+    self.sector = false --sector this body is in
     self.x = 0
     self.y = 0
-
-    self.sector = false --sector this body is in
-    --self.target
-
-    self.color = {random(80, 240), random(80, 240), random(80, 240), 255}
-    self.image = 0 --there is no 0 image
-    self.sx = 1
-    self.sy = 1
-
     self:setHeading("right") --self.r / self.heading
-
+    -- Movement
     self.throttle = 0
     self.speed = 0
     self.acceleration = 0.032 -- 1 pixel per second^2
     self.maxSpeed = 8         -- 100 pixels per second
+    -- Display
+    self.color = {random(80, 240), random(80, 240), random(80, 240), 255}
+    self.image = 0 --there is no 0 image
+    self.sx = 1
+    self.sy = 1
+    -- References/Contains
+    self.target = false --would be reference to targeted body
+        --NOTE POTENTIAL BUG:
+        -- if something is targeting something and it leaves the sector, it is still targeted!
+    self.communication = false
+    self.commsHistory = {}
+    --[[
+    self.modules = {} --these modify capabilities
+    self.storage = {
+        items = {},
+        volume = 0,
+        maxVolume = 100
+    }
+    --]]
 end
 
 function Body:update(dt)
